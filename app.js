690ec7467dfdbca0caec4b813d58bbaf382f4903
ericcalder <ericcalder@me.com>
2019-03-11 23:38:55 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var express=require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var fs = require('fs');
+
+var app = express();
+var index = require('./routes/index');
+var home = require('./routes/home');
+var edit = require('./routes/edit');
+var login = require('./routes/login');
+var cleaners = require('./routes/cleaners');
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/index', index);
+app.use('/home', home);
+app.use('/edit', edit);
+app.use('/login', login);
+app.use('/cleaners', cleaners);
+
+
+app.get('/', function(req,res){
+	console.log('in root');
+res.render('login');
+});
+
+
+
+app.listen(3000);
